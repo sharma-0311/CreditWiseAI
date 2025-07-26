56afcf3e0f6c5131d2c585c6482f6d4ffeae5131
Raghav Gaur <rgour6350@gmail.com>
2025-07-26 12:18:16 +0530

    Database Schema

---
+++ Database/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE applicant_inputs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) DEFAULT 'Anonymous',
+    age INT,
+    credit_amount FLOAT,
+    employment_duration VARCHAR(100),
+    savings_status VARCHAR(100),
+    loan_purpose VARCHAR(100),
+    housing VARCHAR(50),
+    number_of_dependents INT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE prediction_logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    applicant_id INT,
+    is_creditworthy BOOLEAN,
+    confidence_score FLOAT,
+    prediction_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (applicant_id) REFERENCES applicant_inputs(id)
+);
+-- SELECT * FROM applicant_inputs;
+-- SELECT * FROM prediction_logs;
